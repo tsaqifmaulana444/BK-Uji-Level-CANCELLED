e6dd327c08f8f602f07c5bb2afe55c76932c94fd
tsaqifmaulana444 <tsaqifdepok@gmail.com>
2023-05-24 18:20:18 +0700

    update

--- routes/web.php
+++ routes/web.php
@@ -18,24 +18,19 @@ use App\Http\Controllers\FrontEndController;
 
 Route::controller(FrontEndController::class)->group(function(){
     Route::get("/", "index")->name("/");
-});
-
-Route::controller(FrontEndController::class)->group(function(){
     Route::get("/kritik_saran", "kritik_saran")->name("kritik_saran");
-});
-
-Route::controller(FrontEndController::class)->group(function(){
     Route::get("/about_us", "about_us")->name("about_us");
+    Route::get("/contact", "contact_person")->name("contact");
+    Route::get("/news", "berita")->name("news");
 });
 
 Route::controller(LoginController::class)->group(function(){
     Route::get("/login", "index")->name("login");
-});
-
-Route::controller(LoginController::class)->group(function(){
     Route::get("/signup", "sign_up")->name("signup");
 });
 
 Route::controller(AdminController::class)->group(function(){
     Route::get("/admin", "index")->name("admin");
+    Route::get("/news_manage", "news")->name("news_manage");
+    Route::get("/delete_news/{id}", "delete_news")->name("delete_news");
 });
