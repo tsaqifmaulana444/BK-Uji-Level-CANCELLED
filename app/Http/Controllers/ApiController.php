1aeeacbc7e212ccb7b9fe1c4ebfd8583973f72a4
tsaqifmaulana444 <tsaqifdepok@gmail.com>
2023-06-03 17:18:27 +0700

    update

---
+++ app/Http/Controllers/ApiController.php
@@ -0,0 +1,81 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\User;
+use App\Models\Pertemuan;
+use Illuminate\Http\Request;
+use App\Helpers\ApiFormatter;
+
+class ApiController extends Controller
+{
+    public function index()
+    {
+        $data = Pertemuan::all();
+        if ($data) {
+            return ApiFormatter::createApi(200, 'Success', $data);
+        } else {
+            return ApiFormatter::createApi(400, 'Failed');
+        }
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    public function show($name)
+    {
+        $data = Pertemuan::where('nama', '=', $name)->get();
+        if ($data) {
+            return ApiFormatter::createApi(200, 'Success', $data);
+        } else {
+            return ApiFormatter::createApi(400, 'Failed');
+        }
+    }
+
+    public function profile($name)
+    {
+        $data = User::where('name', '=', $name)->get();
+        if ($data) {
+            return ApiFormatter::createApi(200, 'Success', $data);
+        } else {
+            return ApiFormatter::createApi(400, 'Failed');
+        }
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(string $id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, string $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(string $id)
+    {
+        //
+    }
+}
